8a467f1c054d396ea7d6c730fdd497139503cdf0
xuwwenke <3492517331@qq.com>
2019-12-04 10:23:43 +0800

    regiset

--- 项目/前端/src/container/Regiset.js
+++ 项目/前端/src/container/Regiset.js
@@ -7,7 +7,7 @@ export default class Regiset extends Component {
         this.state = {
             phoneNumber: '',
             pws: '',
-            rpws:'',
+            class:'',
             validateCode:''
         }
     }
@@ -23,7 +23,7 @@ export default class Regiset extends Component {
     }
     handleChange2=(e)=>{
         this.setState({
-            rpws: e.target.value
+            class: e.target.value
         })
     }
     handleChange3=(e)=>{
@@ -60,38 +60,90 @@ export default class Regiset extends Component {
     render() {
         const { phoneNumber, validateCode } = this.state;
         return (
-            <div>
-                <NavBar
-                style={{backgroundColor:'blue',color:'white'}}
-                >注册</NavBar>
-                <div style={{width:'100%',marginTop:'30px',backgroundColor:'white'}}>
-                <form action={this.state.url} method="GET" style={{
-                    width:'100%',
-                    height:'200px'
+            <div style={{
+                width:'100%',
+                height:'812px',
+                background:'url(img/mobile-bg.jpg) center center /  100% 100%  no-repeat',
+                position:'relative'
+            }}>
+                <div style={{width:'100%'}}>
+                    <NavBar
+                        style={{backgroundColor:'blue',color:'white'}}
+                    >注册</NavBar>
+                </div>
+                <div style={{
+                    position:'absolute',
+                    width:'90%',
+                    margin:'0 auto',
+                    backgroundColor:'rgb(219, 238, 240)',
+                    height:'330px',
+                    borderRadius:'20px',
+                    top:'15%',
+                    left:'5%',
+                    float:'left'
+                    }}>
+                <form method="GET" style={{
+                    width:'80%',
+                    height:'200px',
+                    position:'relative',
+                    margin:'0 auto'
                 }}>
-                    <input type="text" autocomplete="off" placeholder="手机号" onChange={this.handleChange}  id="phoneNumber" name="phoneNumber" style={{
+                    <input type="text" autocomplete="off" placeholder="手机号" onChange={this.handleChange}  id="username" name="username" style={{
                         width:'100%',
-                        height:'40px'
+                        position:'absolute',
+                        top:'10%',
+                        height:'40px',
+                        border:'solid 1px black',
+                        borderRadius:'10px'
                     }}/>
                     <WhiteSpace/>
-                    <input type="password" autocomplete="off" placeholder="密码" onChange={this.handleChange1}  id="pwd"  name="pwd" style={{
+                    <input type="password" autocomplete="off" placeholder="密码" onChange={this.handleChange2}  id="pwd"  name="pwd" style={{
                         width:'100%',
-                        height:'40px'
+                        height:'40px',
+                        position:'absolute',
+                        top:'35%',
+                        border:'solid 1px black',
+                        borderRadius:'10px'
                     }} />
                     <WhiteSpace/>
-                    <input type="password" autocomplete="off" placeholder="再次输入密码" onChange={this.handleChange2}  id="pwd"  name="rpwd" style={{
+                    <input type="password" autocomplete="off" placeholder="年级" onChange={this.handleChange3}  id="pwd"  name="pwd" style={{
                         width:'100%',
-                        height:'40px'
+                        height:'40px',
+                        position:'absolute',
+                        top:'60%',
+                        border:'solid 1px black',
+                        borderRadius:'10px'
                     }} />
                     <WhiteSpace/>
-                    <input type="password" autocomplete="off" placeholder="验证码" onChange={this.handleChange3}  name="validateCode" style={{
-                        width:'100%',
-                        height:'40px'
-                    }} />
+                    <input type="password" autocomplete="off" placeholder="验证码" onChange={this.handleChange1}  id="class"  name="class" style={{
+                        width:'60%',
+                        height:'40px',
+                        position:'absolute',
+                        top:'85%',
+                        border:'solid 1px black',
+                        borderRadius:'10px'
+                    }} /><button style={{
+                        width:'37%',
+                        height:'40px',
+                        position:'absolute',
+                        top:'85%',
+                        left:'63%',
+                        border:'solid 1px black',
+                        borderRadius:'10px'
+                    }}>获取验证码</button>
                     <WhiteSpace/>
-                    <Link to='/'>
-                        <Button style={{background:'blue',color:'white'}} onClick={this.register.bind(this, phoneNumber, validateCode)}>去登陆</Button>
-                    </Link>
+                    <input type="submit" id="login" value="去登录" style={{
+                        position:'absolute',
+                        top:'120%',
+                        left:'10%',
+                        color:'white',
+                        background:'blue',
+                        border:'1px solid blue',
+                        height:'40px',
+                        width:'80%',
+                        borderRadius:'10px',
+                        margin:'0 auto'
+                    }} onClick={this.check}></input>
                 </form>
                 </div>
             </div>
